e61a03161f67ef2b13f1a5586c014246b7435566
beraia <temuriberaia1@gmail.com>
2023-07-22 22:39:46 +0400

    update layouts

--- TaskManagerApp/Program.cs
+++ TaskManagerApp/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Task}/{action=Add}/{id?}");
 
 app.Run();
